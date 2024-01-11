f0bc129ba8f7ecd1d050a30ee53548f9a9bb7b14
Alhassan Zainab <azainab390@gmail.com>
2024-01-11 22:52:57 +0000

    py-var-annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A type-annotated function that returns the sum of two floats """
+
+
+def add(a: float, b: float) -> float:
+    """adds two floats and returns sum as a float"""
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A type-annotated function that returns a concatenated string """
+import math
+
+
+def floor(n: float) -> int:
+    """this function  returns a concatenated string"""
+    return math.floor(n)
